f094e97f05811307deb59d5ea145d22cfb86feeb
engahmadnahal <ahmadghssannahal@gmail.com>
2022-08-29 23:54:40 +0300

    Trans Files msg , dash

--- lang/en/msg.php
+++ lang/en/msg.php
@@ -1,4 +1,21 @@
 <?php
 return [
- 
+    'success'=>'Done successfully',
+    'error'=>'Something went wrong',
+    'success_create'=>'Created successfully',
+    'errorـcreate'=>'An error occurred during creation',
+    'success_edit'=>'Updated successfully',
+    'errorـedit'=>'An error occurred while updating',
+    'success_delete' => 'Deleted successfully',
+    'error_delete' => 'An error occurred while deleting',
+    'success_action' =>'Operation completed successfully',
+    'error_action' =>'An error occurred while executing the operation',
+    'points_grth_total_level'=>'You have exceeded the number of points at this level',
+    'succes_get' => 'successfully fetched',
+    'error_get' => 'Fetching error',
+    'num_subs_grt_plan' => 'The number of subscriptions exceeded the limit for this plan'  ,
+    'giv_permission' => 'The user has been given permission',
+    'sucess_login' => 'successfully registered',
+    'error_data_login' => 'Wrong login information',
+    'check_email' => 'Check your email'
 ];
