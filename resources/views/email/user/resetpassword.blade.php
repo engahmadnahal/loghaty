f22f238996827171f1012b3566f281d2c47d49f5
engahmadnahal <ahmadghssannahal@gmail.com>
2022-08-30 14:59:03 +0300

    Fix : Mail

--- resources/views/email/user/resetpassword.blade.php
+++ resources/views/email/user/resetpassword.blade.php
@@ -1,13 +1,14 @@
 @component('mail::message')
-# Hello sir, this email contains your password reset code The code is valid for 1 hour only . 
+# Hello sir, this email contains your password reset code The code is valid for 1 hour only 
 
-This code using to reset password when forgot your password .
+This code using to reset password when forgot your password 
 <br>
 # Code : {{$code}}
 
 #If you didn't do anything and I'm not responsible for sending this email please don't do anything
 <br>
-Thanks.<br>
+Thanks 
+<br>
 <br>
 {{ config('app.name') }}
 @endcomponent
